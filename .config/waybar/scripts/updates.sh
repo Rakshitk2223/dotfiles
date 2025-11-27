9ecb8adeb1954523b943b9e1b379205c49918d77
mohak34 <mohaks0408@gmail.com>
2025-11-27 14:35:19 +0530

    fix: dots rewrite

--- .config/waybar/scripts/updates.sh
+++ .config/waybar/scripts/updates.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Check for updates - outputs icon only if updates available, otherwise empty
 
 # Check for Arch package updates
 arch_updates=$(checkupdates 2>/dev/null | wc -l)
@@ -9,14 +10,7 @@ aur_updates=$(yay -Qua 2>/dev/null | wc -l)
 # Total updates
 total_updates=$((arch_updates + aur_updates))
 
-# Create tooltip text
-if [ $total_updates -eq 0 ]; then
-    tooltip="System is up to date"
-    text="0"
-else
-    tooltip="Arch: $arch_updates updates\nAUR: $aur_updates updates\nTotal: $total_updates updates"
-    text="$total_updates"
+# Only output if there are updates (empty output hides the module)
+if [ $total_updates -gt 0 ]; then
+	echo ""
 fi
-
-# Output JSON for Waybar
-echo "{\"text\":\"$text\",\"tooltip\":\"$tooltip\",\"class\":\"updates\"}"
